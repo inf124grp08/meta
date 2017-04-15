9b629c918787ff384442f738a2270f6400f43d16
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-04-14 17:41:06 -0700

    Add sigwinch trap to gsu.sh

--- gsu.sh
+++ gsu.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/expect
+
+# trap sigwinch and pass it to the child we spawned
+# solution from https://ubuntuforums.org/showthread.php?t=865420
+trap {
+  set rows [stty rows]
+  set cols [stty columns]
+  stty rows $rows columns $cols < $spawn_out(slave,name)
+} WINCH
+
 # create a file called password and put your password in it. make sure nobody else can read it.
 set pw [read [open "password"] ]
+
 spawn gsu inf124grp08
 expect "Password: "
 send "$pw"
